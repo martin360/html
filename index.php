cc1766e076300cae21f50fb95bb7ed325ecdf282
martin360 <mr002903@gmail.com>
2016-10-25 08:48:47 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>golf</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--  There are no external links to css or js-->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="dist/css/custom.css">
+  <script src="dist/jquery/jquery.min.js"></script>
+  <script src="dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<ul>	
+  <li><a href="/downloads">downloads</a></li>
+  <li><a href="/phpcode">phpcode</a></li>
+  <li><a href="/javascript">javascript</a></li>
+  <li><a href="contact.html">contact</a></li>
+</ul>
+
+<!-- do not change the following until you have met localhost checkoff #2 -->
+<div class="jumbotron text-center">
+  <h1>Web Page Requirement</h1>
+  <pre>
+  - Solid color background.
+  - Page must use bootstrap
+  - All pages can not have external links to css, js and anything else.
+  - External links must be approved. eg: http://www.w3schools.com/bootstrap/default.asp
+  - Page must inlcude a responsive iframe.
+  - Index will be index.php
+  - No animations on index.php
+  - One custom professional graphic.
+  - index.php must include a  Nato Alphabet flag from the graphics provided.(Nothing custom.)
+  </pre>
+  
+  <p></p>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <div class="container">
+		<h2>Responsive Iframe</h2>
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe  class="embed-responsive-item" name="if" src="intro.html"  ></iframe>
+			</div>
+	</div>
+
+    </div>
+   
+</div>
+
+</body>
+</html>
